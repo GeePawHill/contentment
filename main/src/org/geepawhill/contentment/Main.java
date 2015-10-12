253146f5e917c78137ef98ac3e6373fde12c35de
GeePawHill <geepawhill@geepawhill.org>
2015-10-12 01:02:56 -0400

    Tighten up boilerplate.

--- main/src/org/geepawhill/contentment/Main.java
+++ main/src/org/geepawhill/contentment/Main.java
@@ -1,26 +1,35 @@
 package org.geepawhill.contentment;
-	
+
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
-
-public class Main extends Application {
+public class Main extends Application
+{
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
+	public void start(Stage stage)
+	{
+		try
+		{
+			prepareStage(stage);
+		}
+		catch (Exception e)
+		{
 			e.printStackTrace();
+			Platform.exit();
 		}
 	}
-	
-	public static void main(String[] args) {
+
+	private void prepareStage(Stage stage)
+	{
+		stage.setScene(new Scene(new BorderPane()));
+		stage.show();
+	}
+
+	public static void main(String[] args)
+	{
 		launch(args);
 	}
 }
