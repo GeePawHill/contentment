3c2d46e8b8a219b8460bdcab1d5c62eaef53ae8e
GeePawHill <geepawhill@geepawhill.org>
2015-10-20 22:25:39 -0400

    Two animations added. Ugh.

--- main/src/org/geepawhill/contentment/Main.java
+++ main/src/org/geepawhill/contentment/Main.java
@@ -1,8 +1,10 @@
 package org.geepawhill.contentment;
 
-
+import javafx.animation.Transition;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Bounds;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -11,32 +13,70 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
 
 public class Main extends Application
 {
+	private Pane root;
+	private Text hiMom;
+
 	@Override
 	public void start(Stage stage)
 	{
 		try
 		{
-			Pane root = prepareStage(stage);
-			Text hiMom = new Text(400d,400d,"Hi Mom!");
-			Bounds hiMomBounds = hiMom.getBoundsInParent();
-			Rectangle hiMomRectangle = new Rectangle();
-			hiMomRectangle.setX(hiMomBounds.getMinX());
-			hiMomRectangle.setY(hiMomBounds.getMinY());
-			hiMomRectangle.setWidth(hiMomBounds.getWidth());
-			hiMomRectangle.setHeight(hiMomBounds.getHeight());
-			hiMomRectangle.setFill(Color.TRANSPARENT);
-			hiMomRectangle.setStroke(Color.RED);
+			root = prepareStage(stage);
+			String hiMomValue = "Hi Mom!";
+			hiMom = new Text(400d, 400d, "");
+			Transition transition = new Transition()
+			{
+				{
+					setCycleDuration(Duration.millis(500));
+				}
+
+				@Override
+				protected void interpolate(double frac)
+				{
+					String newText = hiMomValue.substring(0, (int)(frac * hiMomValue.length()));
+					hiMom.setText(newText);
+
+				}
+			};
 			root.getChildren().add(hiMom);
-			root.getChildren().add(hiMomRectangle);
+			transition.setOnFinished(event -> step2(event));
+			transition.play();
+			
+			
 		}
 		catch (Exception e)
 		{
 			e.printStackTrace();
 			Platform.exit();
 		}
+	}
+
+	private void step2(ActionEvent event)
+	{
+		Bounds hiMomBounds = hiMom.getBoundsInParent();
+		
+		Rectangle hiMomRectangle = new Rectangle();
+		hiMomRectangle.setFill(Color.TRANSPARENT);
+		hiMomRectangle.setStroke(Color.RED);
+		hiMomRectangle.setX(hiMomBounds.getMinX());
+		hiMomRectangle.setY(hiMomBounds.getMinY());
+		Transition transition2 = new Transition() {
+			{
+				setCycleDuration(Duration.millis(500));
+			}
+
+			@Override
+			protected void interpolate(double frac)
+			{
+				hiMomRectangle.setWidth(hiMomBounds.getWidth()*frac);
+				hiMomRectangle.setHeight(hiMomBounds.getHeight()*frac);
+			} };
+		root.getChildren().add(hiMomRectangle);
+		transition2.play();
 	}
 
 	private Pane prepareStage(Stage stage)
