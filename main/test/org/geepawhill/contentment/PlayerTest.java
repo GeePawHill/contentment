791dcb9e407397bb03029866a4cd96524403a0e5
GeePawHill <geepawhill@geepawhill.org>
2015-10-26 12:39:34 -0400

    Added first test, lib project.

---
+++ main/test/org/geepawhill/contentment/PlayerTest.java
@@ -0,0 +1,16 @@
+package org.geepawhill.contentment;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlayerTest
+{
+
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
