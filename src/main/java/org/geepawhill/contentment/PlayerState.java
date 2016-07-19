a38733bd8d55630f6a166e92226377002d393c49
GeePawHill <geepawhill@geepawhill.org>
2016-07-19 12:55:31 -0400

    Dead code

--- src/main/java/org/geepawhill/contentment/PlayerState.java
+++
@@ -1,8 +0,0 @@
-package org.geepawhill.contentment;
-
-public interface PlayerState {
-	public PlayerState onPlay(Context context);
-	public PlayerState onPause(Context context);
-	public PlayerState onResume(Context context);
-	public PlayerState onStop(Context context);
-}
