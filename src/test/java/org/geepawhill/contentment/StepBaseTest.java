762d5727a7f10376a1f606dd7e84f1b3c4020098
GeePawHill <geepawhill@geepawhill.org>
2016-09-29 21:00:17 -0400

    Got a test to test a transitionstep.

--- src/test/java/org/geepawhill/contentment/StepBaseTest.java
+++ src/test/java/org/geepawhill/contentment/StepBaseTest.java
@@ -1,7 +1,10 @@
 package org.geepawhill.contentment;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
+import org.geepawhill.contentment.core.Context;
+import org.geepawhill.contentment.core.Step;
+import org.geepawhill.contentment.step.TransitionStep;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +14,10 @@ import javafx.animation.Transition;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
+import javafx.scene.text.Text;
 import javafx.util.Duration;
 
 @RunWith(JfxTestRunner.class)
@@ -31,9 +38,16 @@ public class StepBaseTest
 	public void animationTest()
 	{
 		System.out.println("About to animate.");
+		Pane region = new Pane();
+		region.setMaxSize(1600d, 900d);
+		region.setMinSize(1600d, 900d);
+		Group group = new Group();
+		region.getChildren().add(group);
+		Context context = new Context(group);
+		Text label = new Text(800d,450d,"");
+		
 		Transition animation = new Transition()
 		{
-
 			{
 				setCycleDuration(Duration.millis(3000d));
 			}
@@ -41,12 +55,63 @@ public class StepBaseTest
 			@Override
 			protected void interpolate(double frac)
 			{
-				System.out.println("Interpolate: " + frac);
+				if(frac==0d)
+				{
+					System.out.println("0");
+				}
+				String text = "Hi Mom!";
+				String newText = text.substring(0, (int) (frac * text.length()));
+				label.setText(newText);
+				label.setX(800d-label.getBoundsInParent().getWidth()/2d);
+				label.setY(450d);
+
+				label.setText(newText);
+				System.out.println("Interpolate: " + frac+" "+newText);
 			}
 		};
-
-		playAnimationAndWaitForFinish(animation);
+		TransitionStep step = new TransitionStep(animation);
+		waitForFinish(context,step);
+		assertEquals("Hi Mom!",label.getText());
 		System.out.println("Animation played.");
+	}
+	
+
+	private void waitForFinish(Context context,Step step)
+	{
+		if (Platform.isFxApplicationThread()) throw new IllegalThreadStateException("Cannot be executed on main JavaFX thread");
+		final Thread currentThread = Thread.currentThread();
+		context.onFinished=(new EventHandler<ActionEvent>()
+		{
+
+			@Override
+			public void handle(ActionEvent event)
+			{
+				synchronized (currentThread)
+				{
+					currentThread.notify();
+				}
+			}
+		});
+		Platform.runLater(new Runnable()
+		{
+
+			@Override
+			public void run()
+			{
+				step.play(context);
+			}
+		});
+		synchronized (currentThread)
+		{
+			try
+			{
+				currentThread.wait();
+			}
+			catch (InterruptedException ex)
+			{
+				// somebody interrupted me, OK
+			}
+		}
 	}
 
 	private synchronized void playAnimationAndWaitForFinish(final Animation animation)
