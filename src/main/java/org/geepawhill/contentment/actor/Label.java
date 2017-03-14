df268c32dfd6c9eaa19090223d6721f31ccb871a
GeePawHill <geepawhill@geepawhill.org>
2017-03-14 16:27:58 -0400

    FadeIn moved to new model.

--- src/main/java/org/geepawhill/contentment/actor/Label.java
+++ src/main/java/org/geepawhill/contentment/actor/Label.java
@@ -1,17 +1,15 @@
 package org.geepawhill.contentment.actor;
 
-import org.geepawhill.contentment.core.Context;
+import org.geepawhill.contentment.core.ActorOutliner;
 import org.geepawhill.contentment.core.Sequence;
-import org.geepawhill.contentment.core.StyleId;
 import org.geepawhill.contentment.geometry.Point;
 import org.geepawhill.contentment.jfx.JfxUtility;
 import org.geepawhill.contentment.model.Actor;
 import org.geepawhill.contentment.model.Step;
 import org.geepawhill.contentment.newstep.Entrance;
+import org.geepawhill.contentment.newstep.FadeStep;
 import org.geepawhill.contentment.newstep.LettersStep;
 import org.geepawhill.contentment.outline.KvOutline;
-import org.geepawhill.contentment.step.SubStep;
-import org.geepawhill.contentment.step.TimedSequence;
 import org.geepawhill.contentment.step.TransitionStep;
 import org.geepawhill.contentment.timing.FixedTiming;
 import org.geepawhill.contentment.utility.Names;
@@ -54,32 +52,35 @@ public class Label implements Actor
 		sequence.add(lettersStep);
 	}
 
-	public Step fadeIn(double ms)
+	public void fadeIn(Sequence sequence,double ms)
 	{
-		SubStep[] substeps = new SubStep[]
-		{
-				new SubStep(500d, this::fadeIn)
-		};
-		return new TimedSequence(ms, group, substeps);
+		LettersStep lettersStep = new LettersStep(FixedTiming.INSTANT, source, center, text);
+		group().setOpacity(0d);
+		sequence.add(lettersStep);
+		sequence.add(new Entrance(this));
+		sequence.add(new FadeStep(this,ms) );
 	}
 	
 
 	@Override
 	public void outline(KvOutline output)
 	{
-		
+		ActorOutliner outliner = new ActorOutliner(this, output);
+		outliner.start();
+		outliner.append("Source",source);
+		outliner.startNode(text);
+		if (outliner.visibility(text))
+		{
+			outliner.append("Current",text.getText());
+			outliner.bounds(text);
+			outliner.opacity(text);
+			outliner.strokeWidth(text);
+			outliner.lineColor(text);
+		}
+		outliner.endNode();
+		outliner.end();
 	}
 	
-	protected void fadeIn(double frac, Context context)
-	{
-		if (frac == 0d)
-		{
-			group.setOpacity(0d);
-			animateDrawText(1d, context);
-		}
-		group.setOpacity(frac);
-	}
-
 	public Step move(double newX, double newY)
 	{
 		TranslateTransition transition = new TranslateTransition();
@@ -90,16 +91,6 @@ public class Label implements Actor
 		return new TransitionStep(transition);
 	}
 	
-	protected void animateDrawText(double frac, Context context)
-	{
-		context.apply(StyleId.ShapePen, text);
-		context.styles.get(StyleId.Font).apply(text);
-		String newText = source.substring(0, (int) (frac * source.length()));
-		text.setText(newText);
-		text.setX(center.x - text.getBoundsInParent().getWidth() / 2d);
-		text.setY(center.y);
-	}
-
 	@Override
 	public Group group()
 	{
--- src/main/java/org/geepawhill/contentment/newstep/FadeStep.java
+++ src/main/java/org/geepawhill/contentment/newstep/FadeStep.java
@@ -1,47 +1,59 @@
-package org.geepawhill.contentment.step;
+package org.geepawhill.contentment.newstep;
 
 import org.geepawhill.contentment.core.Context;
+import org.geepawhill.contentment.model.Actor;
 import org.geepawhill.contentment.model.OnFinished;
 import org.geepawhill.contentment.model.Step;
 import org.geepawhill.contentment.model.Timing;
+import org.geepawhill.contentment.step.ContextTransition;
 import org.geepawhill.contentment.timing.FixedTiming;
 
 import javafx.animation.Transition;
-import javafx.util.Duration;
 
-public class TransitionStep implements Step
+public class FadeStep implements Step
 {
-	
+
+	private Actor actor;
+	private double ms;
 	private Transition transition;
-	
-	public TransitionStep(Transition transition)
+
+	public FadeStep(Actor actor, double ms)
 	{
-		this.transition = transition;
-	}
-	
-	@Override
-	public Timing timing()
-	{
-		return new FixedTiming(transition.getTotalDuration().toMillis());
+		this.actor = actor;
+		this.ms = ms;
+
 	}
 
 	@Override
 	public void after(Context context)
 	{
-		transition.jumpTo(Duration.millis(10000d));
+		doFade(1.0d, context);
 	}
 
 	@Override
 	public void before(Context context)
 	{
-		transition.jumpTo(Duration.millis(0));
+		doFade(0.0d, context);
 	}
 
 	@Override
 	public void play(Context context, OnFinished onFinished)
 	{
+		transition = new ContextTransition(context, this::doFade, ms);
 		transition.setOnFinished((event) -> onFinished.run());
 		transition.play();
+	}
+
+	private void doFade(double fraction, Context context)
+	{
+		if (fraction <0.0001d)
+		{
+			actor.group().setOpacity(0d);
+		}
+		else
+		{
+			actor.group().setOpacity(fraction * 1d);
+		}
 	}
 
 	@Override
@@ -54,6 +66,12 @@ public class TransitionStep implements Step
 	public void resume(Context context)
 	{
 		transition.play();
+	}
+
+	@Override
+	public Timing timing()
+	{
+		return new FixedTiming(ms);
 	}
 
 }
