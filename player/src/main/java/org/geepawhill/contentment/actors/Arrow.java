a9da86be6241a166338f588ab13f51fbda3da831
GeePawHill <geepawhill@geepawhill.org>
2017-07-14 15:14:48 -0400

    Moved Arrow to BezierAtom.

--- player/src/main/java/org/geepawhill/contentment/actors/Arrow.java
+++ player/src/main/java/org/geepawhill/contentment/actors/Arrow.java
@@ -4,17 +4,14 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import org.geepawhill.contentment.actor.Actor;
+import org.geepawhill.contentment.atom.BezierAtom;
 import org.geepawhill.contentment.connector.arrow.ArrowComputer;
 import org.geepawhill.contentment.connector.arrow.ArrowPoints;
 import org.geepawhill.contentment.connector.arrow.NodeArrowComputer;
-import org.geepawhill.contentment.core.Context;
-import org.geepawhill.contentment.fast.AddNode;
-import org.geepawhill.contentment.fast.Compute;
 import org.geepawhill.contentment.format.Format;
 import org.geepawhill.contentment.geometry.Bezier;
 import org.geepawhill.contentment.geometry.PointPair;
-import org.geepawhill.contentment.step.BezierStep;
-import org.geepawhill.contentment.step.ShapeStep;
+import org.geepawhill.contentment.step.AtomStep;
 import org.geepawhill.contentment.step.Step;
 import org.geepawhill.contentment.step.Timed;
 import org.geepawhill.contentment.timing.Timing;
@@ -28,11 +25,11 @@ public class Arrow implements Actor
 
 	private final Group group;
 
-	private BezierStep mainStep;
-	private BezierStep fromTopStep;
-	private BezierStep fromBottomStep;
-	private BezierStep toTopStep;
-	private BezierStep toBottomStep;
+	private BezierAtom mainStep;
+	private BezierAtom fromTopStep;
+	private BezierAtom fromBottomStep;
+	private BezierAtom toTopStep;
+	private BezierAtom toBottomStep;
 
 	private boolean pointAtFrom;
 	private boolean pointAtTo;
@@ -40,7 +37,7 @@ public class Arrow implements Actor
 	private ArrowComputer computer;
 	private ArrowPoints points;
 
-	private ArrayList<ShapeStep> steps;
+	private ArrayList<BezierAtom> steps;
 
 	private Random random;
 
@@ -53,20 +50,19 @@ public class Arrow implements Actor
 		this.computer = new NodeArrowComputer(from.group(), to.group());
 		this.group = new Group();
 		steps = new ArrayList<>();
-		mainStep = new BezierStep(Timing.weighted(.9d), format);
-		steps.add(mainStep);
+		mainStep = new BezierAtom(this,this::getMainBezier, format);
 		if (pointAtFrom)
 		{
-			fromTopStep = new BezierStep(Timing.weighted(.1d), format);
+			fromTopStep = new BezierAtom(this,this::getFromTop, format);
 			steps.add(fromTopStep);
-			fromBottomStep = new BezierStep(Timing.weighted(.1d), format);
+			fromBottomStep = new BezierAtom(this,this::getFromBottom, format);
 			steps.add(fromBottomStep);
 		}
 		if (pointAtTo)
 		{
-			toTopStep = new BezierStep(Timing.weighted(.1d), format);
+			toTopStep = new BezierAtom(this,this::getToTop, format);
 			steps.add(toTopStep);
-			toBottomStep = new BezierStep(Timing.weighted(.1d), format);
+			toBottomStep = new BezierAtom(this,this::getToBottom, format);
 			steps.add(toBottomStep);
 		}
 	}
@@ -76,21 +72,32 @@ public class Arrow implements Actor
 		return nickname;
 	}
 
-	private void computePoints(Context context, double fraction)
+	private Bezier getMainBezier()
 	{
 		points = computer.compute();
-		mainStep.changeBezier(chooseBezier(points.main));
-		if (pointAtFrom)
-		{
-			fromTopStep.changeBezier(new Bezier(points.fromTop));
-			fromBottomStep.changeBezier(new Bezier(points.fromBottom));
-		}
-		if (pointAtTo)
-		{
-			toTopStep.changeBezier(new Bezier(points.toTop));
-			toBottomStep.changeBezier(new Bezier(points.toBottom));
-		}
+		return chooseBezier(points.main);
 	}
+	
+	private Bezier getFromTop()
+	{
+		return chooseBezier(points.fromTop);
+	}
+	
+	private Bezier getFromBottom()
+	{
+		return chooseBezier(points.fromBottom);
+	}
+	
+	private Bezier getToTop()
+	{
+		return chooseBezier(points.toTop);
+	}
+	
+	private Bezier getToBottom()
+	{
+		return chooseBezier(points.toBottom);
+	}
+
 	
 	public Bezier chooseBezier(PointPair points)
 	{
@@ -110,11 +117,10 @@ public class Arrow implements Actor
 	public Step draw(double ms)
 	{
 		Timed sequence = new Timed(ms);
-		sequence.add(new Compute(this::computePoints));
-		for (ShapeStep step : steps)
+		sequence.add(new AtomStep(Timing.weighted(.9d),mainStep));
+		for (BezierAtom step : steps)
 		{
-			sequence.add(new AddNode(group, step));
-			sequence.add(step);
+			sequence.add(new AtomStep(Timing.weighted(.1d),step));
 		}
 		return sequence;
 	}
