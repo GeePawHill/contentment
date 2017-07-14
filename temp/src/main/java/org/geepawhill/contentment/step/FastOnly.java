6ed71a5d0867199a1c755276214049482b853e3b
GeePawHill <geepawhill@geepawhill.org>
2017-07-14 17:14:06 -0400

    More noise removed.

--- temp/src/main/java/org/geepawhill/contentment/step/FastOnly.java
+++
@@ -1,9 +0,0 @@
-package org.geepawhill.contentment.step;
-
-import org.geepawhill.contentment.core.Context;
-
-public interface FastOnly
-{
-	public void fast(Context context);
-	public void undo(Context context);
-}
