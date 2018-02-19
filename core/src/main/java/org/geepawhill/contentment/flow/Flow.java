0495ede04e473c17e19477873e4ab8368355f148
GeePaw Hill <geepawhill@geepawhill.org>
2018-02-19 14:08:23 -0500

    Tests for Size and Color

--- core/src/main/java/org/geepawhill/contentment/flow/Flow.java
+++ core/src/main/java/org/geepawhill/contentment/flow/Flow.java
@@ -4,18 +4,30 @@ import java.util.Vector;
 
 public class Flow
 {
-	static class Line
+	public static enum Color
+	{
+		Primary, Secondary, Emphatic
+	};
+
+	public static enum Size
+	{
+		Jumbo, Normal, Small
+	};
+
+	public static class Line
 	{
 		public String text;
+		public Color color;
+		public Size size;
 	}
 
 	private final Vector<Line> lines;
-	
+
 	public Flow()
 	{
 		lines = new Vector<>();
 	}
-	
+
 	public Vector<Line> lines()
 	{
 		return lines;
@@ -24,11 +36,43 @@ public class Flow
 	public void load(String source)
 	{
 		String[] markups = source.split("\n");
-		for(String markup : markups)
+		for (String markup : markups)
 		{
 			Line line = new Line();
 			line.text = markup.substring(2);
+			line.color = chooseColor(markup);
+			line.size = chooseSize(markup);
 			lines.add(line);
+		}
+	}
+
+	private Color chooseColor(String substring)
+	{
+		char colorChar = substring.toLowerCase().charAt(0);
+		switch (colorChar)
+		{
+		default:
+		case 'P':
+			return Color.Primary;
+		case 's':
+			return Color.Secondary;
+		case 'e':
+			return Color.Emphatic;
+		}
+	}
+
+	private Size chooseSize(String substring)
+	{
+		char sizeChar = substring.toLowerCase().charAt(1);
+		switch (sizeChar)
+		{
+		default:
+		case 'n':
+			return Size.Normal;
+		case 'j':
+			return Size.Jumbo;
+		case 's':
+			return Size.Small;
 		}
 	}
 }
--- core/src/test/java/org/geepawhill/contentment/flow/FlowTest.java
+++ core/src/test/java/org/geepawhill/contentment/flow/FlowTest.java
@@ -1,24 +1,41 @@
 package org.geepawhill.contentment.flow;
 
-import static org.junit.Assert.*;
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class FlowTest
 {
 
+	private Flow flow;
+	
+	@Before
+	public void before()
+	{
+		flow = new Flow();
+	}
+
 	@Test
 	public void linesHaveCorrectString()
 	{
-		Flow flow = new Flow();
 		flow.load("pjThis is primary jumbo.\n"
 				+ "snThis is secondary normal.\n"
-				+ "pnThis is prinary norma.\n"
-				+ "ejThis is emphatic jumbo.\n");
+				+ "xnThis is primary normal.\n"
+				+ "esThis is emphatic small.\n");
 		assertThat(flow.lines().size()).isEqualTo(4);
-		assertThat(flow.lines().get(0).text).isEqualTo("This is primary jumbo.");
-		
+		assertLine(0,"This is primary jumbo.", Flow.Color.Primary, Flow.Size.Jumbo);
+		assertLine(1,"This is secondary normal.", Flow.Color.Secondary, Flow.Size.Normal);
+		assertLine(2,"This is primary normal.", Flow.Color.Primary, Flow.Size.Normal);
+		assertLine(3,"This is emphatic small.", Flow.Color.Emphatic, Flow.Size.Small);
+	}
+	
+	public void assertLine(int index,String text, Flow.Color color, Flow.Size size)
+	{
+		Flow.Line line = flow.lines().get(index);
+		assertThat(line.text).isEqualTo(text);
+		assertThat(line.color).isEqualTo(color);
+		assertThat(line.size).isEqualTo(size);
 	}
 
 }
